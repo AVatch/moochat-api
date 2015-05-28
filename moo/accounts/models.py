b70a42e66a2230c9458b8f8fa5d0f4734bd44e20
Adrian Vatchinsky <avatchinsky@Adrians-MacBook-Pro.local>
2015-05-27 21:02:36 -0400

    django rest application with basic accounts application

---
+++ moo/accounts/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.conf import settings
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.dispatch import receiver
+
+from rest_framework.authtoken.models import Token
+
+
+class AccountManager(BaseUserManager):
+    def create_user(self, password=None, **kwargs):
+        if not kwargs.get('username'):
+            raise ValueError('User must have a valid username')
+
+        account = self.model(
+            username=kwargs.get('username'),
+        )
+
+        if password is None:
+            raise ValueError('User must have a valid password')
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, password, **kwargs):
+        account = self.create_user(password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
+
+
+class Account(AbstractBaseUser):
+    username = models.CharField(max_length=40, unique=True)
+    profile_picture_url = models.URLField(default="http://placehold.it/150x150")
+    first_name = models.CharField(max_length=60, blank=True)
+    last_name = models.CharField(max_length=60, blank=True)
+    phonenumber = models.CharField(max_length=15, blank=True)
+
+    is_admin = models.BooleanField(default=False)
+    is_manager = models.BooleanField(default=False)
+    time_created = models.DateTimeField(auto_now_add=True)
+    time_updated = models.DateTimeField(auto_now=True)
+
+    # Define custom manager
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        ordering = ('time_created', )
+
+    def __unicode__(self):
+        return self.username
+
+    def get_full_name(self):
+        return ' '.join([self.first_name, self.last_name])
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """
+    Generate a token every time a new account object
+    is created.
+    """
+    if created:
+        Token.objects.create(user=instance)
---
+++ moo/accounts/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        extra_kwargs = {'password': {'write_only': True, 'allow_blank': True}}
+        read_only_fields = ('last_login',)
+
+    def create(self, validated_data):
+        account = Account(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        account.set_password(validated_data['password'])
+        account.save()
+        return account
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name',
+                                                 instance.first_name)
+        instance.last_name = validated_data.get('last_name',
+                                                instance.last_name)
+
+        if validated_data['password'] == '':
+            pass
+        else:
+            instance.set_password(validated_data['password'])
+
+        instance.save()
+
+        return instance
