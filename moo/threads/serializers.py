bf67c567e9bee57c87168adc4c2ee63ec5079495
AVatch <avatchinsky@outlook.com>
2015-05-29 09:35:19 -0400

    threads display fully serialized accounts

--- moo/threads/serializers.py
+++ moo/threads/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+from accounts.serializers import AccountSerializer
+
 from .models import Thread
 
 class ThreadSerializer(serializers.ModelSerializer):
+    participants = AccountSerializer(many=True)
     class Meta:
         model = Thread
           
