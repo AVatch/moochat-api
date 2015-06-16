de01de3730b6c56f2a89770e4dd5b49dcf0f70df
AVatch <avatchinsky@outlook.com>
2015-06-16 11:13:53 -0400

    configure beanstalk

---
+++ moo/accounts/management/commands/createsu.py
@@ -0,0 +1,10 @@
+import os
+
+from django.core.management.base import BaseCommand
+from accounts.models import Account
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not Account.objects.filter(username="admin").exists():
+            Account.objects.create_superuser("admin", "admin@admin.com", "admin")
--- moo/moo/settings.py
+++ moo/moo/settings.py
@@ -97,12 +97,23 @@ WSGI_APPLICATION = 'moo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
 
 
@@ -123,6 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
 
 # Define custom User Object
